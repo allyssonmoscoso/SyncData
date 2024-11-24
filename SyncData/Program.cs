85013c3782f09a7455850ac81df40bbbb530270d
Allysson Jesus Moscoso <allysson-moscoso500@outlook.com>
2024-11-24 16:23:28 -0400

    add: Validacion cuando son las mias rutas

--- SyncData/Program.cs
+++ SyncData/Program.cs
@@ -5,11 +5,22 @@ class SyncData
 {
     public static void Main()
     {
-        Console.Write("Ingrese la primera ruta de directorio: ");
-        string path1 = Console.ReadLine();
+        string path1;
+        string path2;
+        Console.WriteLine("Este programa sincroniza dos directorios, copiando los archivos que no existan en el directorio de destino o que hayan sido modificados en el directorio fuente.");
 
+        Console.Write("Ingrese la primera ruta de directorio: ");
+        path1 = Console.ReadLine();
+
+        do
+        {
         Console.Write("Ingrese la segunda ruta de directorio: ");
-        string path2 = Console.ReadLine();
+            path2 = Console.ReadLine();
+            if (path1 == path2)
+            {
+                Console.WriteLine("La segunda ruta no puede ser igual a la primera. Intente nuevamente.");
+            }
+        } while (path1 == path2);
 
         if (Directory.Exists(path1) && Directory.Exists(path2))
         {

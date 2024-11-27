cdeaf31d206054e3360518715c0935abf9e68b13
Allysson Jesus Moscoso <allysson-moscoso500@outlook.com>
2024-11-27 02:10:19 -0400

    refactor: Move SynchronizeDirectories method to Utility class for improved organization

--- SyncData/Program.cs
+++ SyncData/Program.cs
@@ -61,7 +61,7 @@ namespace SyncData
 
         if (Directory.Exists(path1) && Directory.Exists(path2))
         {
-            SynchronizeDirectories(path1, path2, verbose, logToFile);
+            Utility.SynchronizeDirectories(path1, path2, verbose, logToFile);
             if (verbose)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -81,111 +81,7 @@ namespace SyncData
             }
 
             Utility.LogMessage("Error", "One or both paths do not exist.", verbose, logToFile);
-        }
-      
-        static void SynchronizeDirectories(string sourceDir, string targetDir, bool verbose, bool logToFile)
-        {
-            var sourceDirectory = new DirectoryInfo(sourceDir);
-            var targetDirectory = new DirectoryInfo(targetDir);
-
-            var filesToCopy = sourceDirectory.GetFiles().Length + targetDirectory.GetFiles().Length;
-            var directoriesToCreate = sourceDirectory.GetDirectories().Length + targetDirectory.GetDirectories().Length;
-            var totalOperations = filesToCopy + directoriesToCreate;
-
-            using (var progressBar = new ProgressBar())
-            {
-                int progress = 0;
-
-                // Synchronize files from source directory to target directory
-                foreach (var file in sourceDirectory.GetFiles())
-                {
-                    var targetFilePath = Path.Combine(targetDir, file.Name);
-                    if (!File.Exists(targetFilePath) || file.LastWriteTime > File.GetLastWriteTime(targetFilePath))
-                    {
-                        var startTime = DateTime.Now;
-                        file.CopyTo(targetFilePath, true);
-                        var endTime = DateTime.Now;
-                        if (verbose)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine($"File synchronized: {file.FullName} -> {targetFilePath} (Time: {(endTime - startTime).TotalMilliseconds} ms)");
-                            Console.ResetColor();
-                        }
-                        Utility.LogMessage("Success", $"File synchronized: {file.FullName} -> {targetFilePath} (Time: {(endTime - startTime).TotalMilliseconds} ms)", verbose, logToFile);
-                    }
-
-                    progress++;
-                    progressBar.Report((double)progress / totalOperations);
-                }
-
-                // Synchronize files from target directory to source directory
-                foreach (var file in targetDirectory.GetFiles())
-                {
-                    var sourceFilePath = Path.Combine(sourceDir, file.Name);
-                    if (!File.Exists(sourceFilePath) || file.LastWriteTime > File.GetLastWriteTime(sourceFilePath))
-                    {
-                        var startTime = DateTime.Now;
-                        file.CopyTo(sourceFilePath, true);
-                        var endTime = DateTime.Now;
-                        if (verbose)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.WriteLine($"File synchronized: {file.FullName} -> {sourceFilePath} (Time: {(endTime - startTime).TotalMilliseconds} ms)");
-                            Console.ResetColor();
-                        }
-                        Utility.LogMessage("Success", $"File synchronized: {file.FullName} -> {sourceFilePath} (Time: {(endTime - startTime).TotalMilliseconds} ms)", verbose, logToFile);
-                    }
-
-                    progress++;
-                    progressBar.Report((double)progress / totalOperations);
-                }
-
-                // Synchronize subdirectories
-                foreach (var directory in sourceDirectory.GetDirectories())
-                {
-                    var targetSubDirPath = Path.Combine(targetDir, directory.Name);
-                    if (!Directory.Exists(targetSubDirPath))
-                    {
-                        var startTime = DateTime.Now;
-                        Directory.CreateDirectory(targetSubDirPath);
-                        var endTime = DateTime.Now;
-                        if (verbose)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine($"Directory created: {targetSubDirPath} (Time: {(endTime - startTime).TotalMilliseconds} ms)");
-                            Console.ResetColor();
-                        }
-                        Utility.LogMessage("Success", $"Directory created: {targetSubDirPath} (Time: {(endTime - startTime).TotalMilliseconds} ms)", verbose, logToFile);
-                    }
-
-                    SynchronizeDirectories(directory.FullName, targetSubDirPath, verbose, logToFile);
-                    progress++;
-                    progressBar.Report((double)progress / totalOperations);
-                }
-
-                foreach (var directory in targetDirectory.GetDirectories())
-                {
-                    var sourceSubDirPath = Path.Combine(sourceDir, directory.Name);
-                    if (!Directory.Exists(sourceSubDirPath))
-                    {
-                        var startTime = DateTime.Now;
-                        Directory.CreateDirectory(sourceSubDirPath);
-                        var endTime = DateTime.Now;
-                        if (verbose)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine($"Directory created: {sourceSubDirPath} (Time: {(endTime - startTime).TotalMilliseconds} ms)");
-                            Console.ResetColor();
-                        }
-                        Utility.LogMessage("Success", $"Directory created: {sourceSubDirPath} (Time: {(endTime - startTime).TotalMilliseconds} ms)", verbose, logToFile);
-                    }
-
-                    SynchronizeDirectories(directory.FullName, sourceSubDirPath, verbose, logToFile);
-                    progress++;
-                    progressBar.Report(1.0);
-                }
-            }
-        }
+        }     
     }
 }  
 }
